b885b6e56a186626f358925758761244832ca4c6
malivartti <malivartti@yandex.ru>
2024-11-09 00:19:38 +0300

    fix env key

--- module/mail.js
+++ module/mail.js
@@ -3,14 +3,14 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
      auth: {
-        user: process.env.MAIN_USER,
-        pass: process.env.MAIN_PASS,
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
       },
 });
 
 function getMailData(to, token) {
   return {
-    from: process.env.MAIN_USER,
+    from: process.env.MAIL_USER,
       to,
       subject: 'Код для входа в чат',
       text: `Задание 3\ntoken=${token}`,
