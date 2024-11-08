129332e25d675ed435b992881a9066ac2a21d557
malivartti <malivartti@yandex.ru>
2024-11-09 00:22:56 +0300

    add exeption handling for messages

--- httpcontroller/message.js
+++ httpcontroller/message.js
@@ -4,11 +4,19 @@ const { getMessages } = require('../service/message')
 
 router.route('/')
 .get(async (req, res) => {
-  const messages = await getMessages()
-  return res.status(200).send({
-    status: 'ok',
-    messages
-  })
+  try {
+    const messages = await getMessages()
+    return res.status(200).send({
+      status: 'ok',
+      messages
+    })
+  } catch (e) {
+    return res.status(500).send({
+      error: e,
+      status: 'error',
+      message: 'Ошибка при получении сообщений'
+    })
+  }
 })
 
 module.exports = router
